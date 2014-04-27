2e07ee0adfe31b378b984abe70a30d6d892212c2
Chris Mikelson <chris.mikelson@640labs.com>
2014-04-27 12:11:38 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Epicstemeu::Application.config.secret_key_base = '3c8fe6d2e0c3b118111ae3fb1251851ac0268a353a13d0013dda7c1bef418df7a458a6836a41d03e9fba32abadc28fa25467fa3b3c2a9480a2f7991c2a91aeca'
